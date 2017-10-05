758aaf920a5defd7ee4f203744f1156a6ad240a7
Nicolas Hodicq <nartawak@MacBook-Pro-de-Nicolas.local>
2017-10-05 23:05:12 +0200

    Init project

--- src/main/java/org/gestion/web/controller/RestGradeController.java
+++ src/main/java/org/gestion/web/controller/RestGradeController.java
@@ -1,44 +1,88 @@
 package org.gestion.web.controller;
 
-import java.util.List;
-
 import org.gestion.entite.Grade;
-import org.gestion.repository.GradeRepository;
-import org.gestion.services.GradeService;
+import org.gestion.services.IGradeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/grades")
 public class RestGradeController {
 
 	@Autowired
-	private GradeService gradeService;
-	
+	@Qualifier("gradeServiceJpa")
+	private IGradeService gradeServiceJpa;
+
 	@Autowired
-	private GradeRepository gradeRepository;
-	
-	@RequestMapping(path="/withJpa", method = RequestMethod.GET)
-	@ResponseBody
-	public List<Grade> listeEmployesWithJpa() {
-		return gradeService.lister();
-	}
-	
-	@RequestMapping(path="/withRepository", method = RequestMethod.GET)
-	@ResponseBody
-	public List<Grade> listeEmployesWithRepository() {
-		return gradeRepository.findAll();
-	}
-	
-	@RequestMapping(method = RequestMethod.POST, path = "/creer", consumes = "application/json;charset=UTF-8")
-	public String ajoutAbsence(@RequestBody Grade newGrade) {
-		gradeService.sauvegarder(newGrade);
-		return "{'success': true}";
+	@Qualifier("gradeServiceRepository")
+	private IGradeService gradeServiceRepository;
 
+
+
+	// ********************************** //
+	// ******* GET LIST GRADES ********** //
+	// ********************************** //
+
+	@RequestMapping(path = "/withJpa", method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	public List<Grade> getGradesWithJPA() {
+		return gradeServiceJpa.getGrades();
 	}
 
+	@RequestMapping(path = "/withRepository", method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	public List<Grade> getGradesWithRepository() {
+		return gradeServiceRepository.getGrades();
+	}
+
+	// *********************************** //
+	// ******* GET GRADES BY ID ********** //
+	// *********************************** //
+
+	@RequestMapping(path = "/{idGrade}/path-param", method = RequestMethod.GET)
+	@ResponseBody
+	public Grade getGradeByIdWithPathParam(@PathVariable("idGrade") String idGrade) {
+		return gradeServiceRepository.getGradeById(Integer.parseInt(idGrade));
+	}
+
+	@RequestMapping(path = "/query-param", method = RequestMethod.GET)
+	@ResponseBody
+	public Grade getGradeByIdWithQueryParam(@RequestParam("idGrade") String idGrade) {
+		return gradeServiceRepository.getGradeById(Integer.parseInt(idGrade));
+	}
+
+	// *********************************** //
+	// ********** CREATE GRADES ********** //
+	// *********************************** //
+
+	@RequestMapping(path = "/", method = RequestMethod.POST, consumes = "application/json;charset=UTF-8")
+	public Grade createGrade(@RequestBody Grade newGrade) {
+		return gradeServiceJpa.create(newGrade);
+	}
+
+	// *********************************** //
+	// ******* UPDATE GRADE BY ID ******** //
+	// *********************************** //
+
+	@RequestMapping(path = "/", method = RequestMethod.PUT, consumes = "application/json;charset=UTF-8")
+	@ResponseBody
+	public void updateGrade(@RequestBody Grade updateGrade) {
+		gradeServiceRepository.update(updateGrade);
+	}
+
+
+	//
+	// *********************************** //
+	// ******* DELETE GRADE BY ID ******** //
+	// *********************************** //
+
+	@RequestMapping(path = "/{idGrade}", method = RequestMethod.DELETE)
+	@ResponseBody
+	public void deleteGrade(@PathVariable("idGrade") String idGrade) {
+		gradeServiceRepository.deleteGrade(Integer.parseInt(idGrade));
+
+	}
 }
